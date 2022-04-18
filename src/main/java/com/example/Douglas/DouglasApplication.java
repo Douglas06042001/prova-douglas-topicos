ac7805f27f96176d9715f2b898323293f5eb1898
Douglas06042001 <silvestredouglas98@gmail.com>
2022-04-17 22:58:27 -0300

    First Commit

---
+++ src/main/java/com/example/Douglas/DouglasApplication.java
@@ -0,0 +1,14 @@
+package com.example.Douglas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouglasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DouglasApplication.class, args);
+	}
+gfrgfgf
+
+}
